f6927557a0f7f30286d199b35fb1d65228796e38
HughOwenPanopio <panopiohughowen@gmail.com>
2023-09-03 23:55:17 +0800

    Photography Portfolio - MP1

---
+++ JS/Booking.js
@@ -0,0 +1,12 @@
+// scripts.js
+const openButton = document.getElementById('openButton');
+const closeButton = document.getElementById('closeButton');
+const popupForm = document.getElementById('popupForm');
+
+openButton.addEventListener('click', () => {
+    popupForm.style.display = 'block';
+});
+
+closeButton.addEventListener('click', () => {
+    popupForm.style.display = 'none';
+});
